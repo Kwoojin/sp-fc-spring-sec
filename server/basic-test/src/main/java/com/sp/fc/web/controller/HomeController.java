41a45eee07c84fdc652f9088cc66cae414a4083c
김우진 <dnwls488@naver.com>
2022-08-15 13:40:57 +0900

    #3 - login init
    fc service full

--- server/basic-test/src/main/java/com/sp/fc/web/controller/HomeController.java
+++ server/basic-test/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.sp.fc.web.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,18 +30,18 @@ public class HomeController {
      */
     @PreAuthorize("hasAuthority('ROLE_USER')")
     @RequestMapping("/user")
-    public SecurityMessage user() {
+    public SecurityMessage user(@AuthenticationPrincipal UserDetails user) {
         return SecurityMessage.builder()
-                .auth(SecurityContextHolder.getContext().getAuthentication())
+                .user(user)
                 .message("유저 정보")
                 .build();
     }
 
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @RequestMapping("/admin")
-    public SecurityMessage admin() {
+    public SecurityMessage admin(@AuthenticationPrincipal UserDetails user) {
         return SecurityMessage.builder()
-                .auth(SecurityContextHolder.getContext().getAuthentication())
+                .user(user)
                 .message("관리자 정보")
                 .build();
     }
